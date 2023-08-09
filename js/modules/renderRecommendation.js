06865a41a1b8b81c31928d02b98144afa386d1a1
Stephgarro <109922003+Stephgarro@users.noreply.github.com>
2023-08-08 20:31:35 -0600

    feat(css): add new styles

--- js/modules/renderRecommendation.js
+++ js/modules/renderRecommendation.js
@@ -17,11 +17,13 @@
 function renderRecommendation(recommendation) {
   const recommendationDiv = document.querySelector('.plant-info');
 
-  recommendationDiv.innerHTML = `
+  recommendationDiv.innerHTML = `<div class="title__container">
   <p>The perfect plant for you is...</p>
     <h2>${recommendation.name}</h2>
+    </div>
     <img class="plant__image" src="${recommendation.image}" alt="" />
-    <img class="plant__image image__position" src="${recommendation.potImage}" alt="Pot Image" /> <!-- Agrega esta línea para mostrar la imagen de la maceta -->
+    <img class="plant__image image__position" src="${recommendation.potImage}" alt="Pot Image" /> 
+    <div class="characteristics__container">
     <ul>
       <li>Name: ${recommendation.name}</li>
       <li>Soil: ${recommendation.soil}</li>
@@ -29,6 +31,7 @@ function renderRecommendation(recommendation) {
       <li>Color: ${recommendation.color}</li>
       <li>Extras: ${recommendation.extras.join(', ')}</li>
     </ul>
+    </div>
   `;
 }
 
