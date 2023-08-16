c64a3a17cf269d9a9bc8fea585169fcefa4c6191
Stephgarro <109922003+Stephgarro@users.noreply.github.com>
2023-08-15 19:43:28 -0600

    feat(js ): add some functions

--- js/modules/buildRecommendation.js
+++ js/modules/buildRecommendation.js
@@ -1,48 +1,5 @@
-// import PlantsBuilder from '../plantsBuilder.js';
-// import plantsConfig from './config.js';
-
-// function getPlant(place, toxicPlant) {
-//   if (!plantsConfig[place]) {
-//     return null;
-//   }
-
-//   const plantCategory = toxicPlant.toLowerCase() === 'yes' ? 'nonToxic' : 'toxic';
-//   const plantArray = plantsConfig[place][plantCategory];
-//   const plantType = plantArray ? plantArray[0] : null;
-//   const plant = plantType ? { name: plantType.name, image: plantType.image } : null;
-
-//   return plant;
-// }
-
-// function buildRecommendation(place, soilType, toxicPlant, water, style, extras) {
-//   const plant = getPlant(place, toxicPlant);
-  
-//   if (!plant) {
-//     return null;
-//   }
-
-//   const recommendation = new PlantsBuilder()
-//     .withPlant(plant)
-//     .withImage(plant.image)
-//     .withSoilType(soilType)
-//     .withCeramicMaterial() 
-//     .withWateringMethod(water)
-//     .withPotStyle(style)
-//     .withExtras(extras);
-
-//     if (water === 'Overwater') {
-//       recommendation.withClayMaterial(); 
-//     } else if (water === 'Underwater' || water === 'Neither') {
-//       recommendation.withCeramicMaterial();
-//     }
-
-//   localStorage.setItem('plantRecommendation', JSON.stringify(recommendation));
-
-//   return recommendation;
-// }
 
 
-// export default buildRecommendation;
 
 import PlantsBuilder from '../plantsBuilder.js';
 import plantsConfig from './config.js';
@@ -67,15 +24,17 @@ function buildRecommendation(place, soilType, toxicPlant, water, style, extras) 
     return null;
   }
 
+  const extraImages = getExtrasImages(extras);
+
   const recommendation = new PlantsBuilder()
-  .withPlant(plant)
-  .withImage(plant.image)
-  .withSoilType(soilType)
-  .withCeramicMaterial()
-  .withWateringMethod(water)
-  .withPotStyle(style)
-  .withExtras(extras)
-  .withExtrasImages(getExtrasImages(extras));
+    .withPlant(plant)
+    .withImage(plant.image)
+    .withSoilType(soilType)
+    .withCeramicMaterial()
+    .withWateringMethod(water)
+    .withPotStyle(style)
+    .withExtras(extras)
+    .withExtrasImages(extraImages);
 
   if (water === 'Overwater') {
     recommendation.withClayMaterial(); 
@@ -91,16 +50,27 @@ function buildRecommendation(place, soilType, toxicPlant, water, style, extras) 
 
 function getExtrasImages(extras) {
   const extrasImages = [];
-  if (extras.includes("Moss pole")) {
-    extrasImages.push('./images/moss-pole.png');
+  const imagesMap = {
+    "moss-pole": './images/moss-pole.png',
+    "pebbles": './images/pebbles.png',
+    "mini-plants": './images/mini-plants.png',
+  };
+
+  if (extras.length === 0) {
+    return [];
   }
-  if (extras.includes("Pebbles")) {
-    extrasImages.push('./images/pebbles.png');
-  }
-  if (extras.includes("Smaller plants")) {
-    extrasImages.push('./images/mini-plants.png');
-  }
+
+  extras.forEach(extra => {
+    extrasImages.push(imagesMap[extra]);
+  });
+
   return extrasImages;
 }
 
+
+
+
+
 export default buildRecommendation;
+
+
