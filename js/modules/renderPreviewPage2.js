360d526dcd414fe464c95a3373c013fe9d29db69
Vale <valeriams13@gmail.com>
2023-08-17 18:29:59 -0600

    feat(js): edit preview card

--- js/modules/renderPreviewPage2.js
+++ js/modules/renderPreviewPage2.js
@@ -2,19 +2,24 @@ function renderRecommendationPage2(recommendation) {
   const recommendationDiv = document.querySelector('.plant-info');
 
   recommendationDiv.innerHTML = `
-    <div class="plant__title">
-      <p>Preview</p>
-    </div>
-    <img class="plant__image plant__preview" src="${recommendation.image}" alt="" />
-    <img class="plant__image image__position pot__position" src="${recommendation.potImage}" alt="" />
-    <ul class="plant__details card-details--margin">
-      <li>Name: ${recommendation.name}</li>
-      <li>Soil: ${recommendation.soil}</li>
-      <li>Pot: ${recommendation.pot}</li>
-      <li>Color: ${recommendation.color}</li>
-      <li>Extras: ${recommendation.extras.join(', ')}</li>
-    </ul>
-    <a href="" id="customize-btn" class="get-btn avaibility-btn">Check store avaibility</a>
+  <div class="plant__title">
+    <p>Preview</p>
+    <h2>${recommendation.name}</h2>
+  </div>
+  <img class="plant__image" src="${recommendation.image}" alt="" />
+  <img class="plant__image image__position" src="${recommendation.potImage}" alt="" />
+  <img class="soil__image" src="${recommendation.soilImage}" alt="" />
+  ${recommendation.extrasImages
+    .map((image) => `<img class="extra__image ${image.className}" src="${image.src}" alt="" />`)
+    .join('')}
+  <ul class="plant__details">
+    <li>Name: ${recommendation.name}</li>
+    <li>Soil: ${recommendation.soil}</li>
+    <li>Pot: ${recommendation.pot}</li>
+    <li>Color: ${recommendation.color}</li>
+    <li>Extras: ${recommendation.extras.join(', ')}</li>
+  </ul>
+  <a href="" id="customize-btn" class="get-btn avaibility-btn">Check store avaibility</a>
   `;
 }
 
