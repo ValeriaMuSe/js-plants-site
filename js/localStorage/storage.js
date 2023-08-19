5fa2ec103b132e429266ff273afa65f9810feeb8
Vale <valeriams13@gmail.com>
2023-08-19 15:48:17 -0600

    feat(js): soil image in card preview

--- js/localStorage/storage.js
+++ js/localStorage/storage.js
@@ -1,3 +1,21 @@
+// // recommendationStorage.js
+
+// // Function to store the recommendation in local storage
+// function storeRecommendation(recommendation) {
+//   localStorage.setItem('recommendation', JSON.stringify(recommendation));
+// }
+
+// // Function to retrieve the recommendation from local storage
+// function getRecommendation() {
+//   const recommendationJSON = localStorage.getItem('recommendation');
+//   if (recommendationJSON) {
+//     return JSON.parse(recommendationJSON);
+//   }
+//   return null;
+// }
+
+// export { storeRecommendation, getRecommendation};
+
 // recommendationStorage.js
 
 // Function to store the recommendation in local storage
@@ -9,7 +27,9 @@ function storeRecommendation(recommendation) {
 function getRecommendation() {
   const recommendationJSON = localStorage.getItem('recommendation');
   if (recommendationJSON) {
-    return JSON.parse(recommendationJSON);
+    const recommendation = JSON.parse(recommendationJSON);
+    console.log('soilImage:', recommendation.soilImage); // Add console.log statement
+    return recommendation;
   }
   return null;
 }
--- js/modules/formLogic.js
+++ js/modules/formLogic.js
@@ -1,6 +1,42 @@
 import buildRecommendation from './buildRecommendation.js';
 import { renderRecommendation, clearRecommendation } from './renderRecommendation.js';
 
+// const initializeFormLogic = () => {
+//   const form = document.getElementById('plants-form');
+
+//   form.addEventListener('submit', event => {
+//     event.preventDefault();
+
+//     const place = document.querySelector('input[name="place"]:checked').value;
+//     const soilType = document.querySelector('input[name="soilType"]:checked').value;
+//     const toxicPlant = document.querySelector('input[name="toxicPlant"]:checked').value;
+//     const water = document.querySelector('input[name="water"]:checked').value;
+//     const stylePot = document.querySelector('input[name="stylePot"]:checked').value;
+   
+   
+  
+//   const extras = Array.from(document.querySelectorAll('input[name="extras"]:checked')).map(input => input.value);
+
+//   if (!extras) {
+//     extras = [];
+//   }
+
+ 
+//   const recommendation = buildRecommendation(place, soilType, toxicPlant, water, stylePot, extras, );
+//   renderRecommendation(recommendation);
+// });
+
+//   const clearButton = document.querySelector('.clear-btn');
+
+//   clearButton.addEventListener('click', () => {
+//     form.reset();
+//     clearRecommendation();
+//   });
+// };
+
+// export default initializeFormLogic;
+
+// Module where initializeFormLogic function is defined
 const initializeFormLogic = () => {
   const form = document.getElementById('plants-form');
 
@@ -12,19 +48,19 @@ const initializeFormLogic = () => {
     const toxicPlant = document.querySelector('input[name="toxicPlant"]:checked').value;
     const water = document.querySelector('input[name="water"]:checked').value;
     const stylePot = document.querySelector('input[name="stylePot"]:checked').value;
-   
-   
-  
-  const extras = Array.from(document.querySelectorAll('input[name="extras"]:checked')).map(input => input.value);
+    const extras = Array.from(document.querySelectorAll('input[name="extras"]:checked')).map(input => input.value);
 
-  if (!extras) {
-    extras = [];
-  }
+    // Capture the soilImage value based on the selected soilType
+    let soilImageSrc;
+    if (soilType === 'soil-composted') {
+      soilImageSrc = "../../images/soil-composted.png";
+    } else if (soilType === 'soil-fertilized') {
+      soilImageSrc = "../../images/soil-fertilized.png";
+    }
 
- 
-  const recommendation = buildRecommendation(place, soilType, toxicPlant, water, stylePot, extras, );
-  renderRecommendation(recommendation);
-});
+    const recommendation = buildRecommendation(place, soilType, toxicPlant, water, stylePot, extras, soilImageSrc);
+    renderRecommendation(recommendation);
+  });
 
   const clearButton = document.querySelector('.clear-btn');
 
