d48973c230d1f05d8817291327856105d4532299
Vale <valeriams13@gmail.com>
2023-08-21 04:48:29 -0600

    feat(js): update customize plant

--- js/customize-plant.js
+++ js/customize-plant.js
@@ -1,33 +1,3 @@
 import renderRecommendationPage2 from './modules/renderPreviewPage2.js'
 
 renderRecommendationPage2()
-
-import observer from './modules/form-handler.js';
-
-// class PreviewSubscriber {
-//     constructor(elementToUpdate) {
-//         this.element = elementToUpdate;
-//     }
-
-//     update(data) {
-//         // Actualiza la previsualización de la planta usando los datos proporcionados
-//         // Puedes manipular la imagen, el título, etc., dentro de este método
-//     }
-// }
-
-// class InfoSubscriber {
-//     constructor(elementToUpdate) {
-//         this.element = elementToUpdate;
-//     }
-
-//     update(data) {
-//         // Actualiza la información de la orden usando los datos proporcionados
-//         // Puedes manipular el texto con los detalles de la orden dentro de este método
-//     }
-// }
-
-// const previewSubscriber = new PreviewSubscriber(/* referencia al elemento */);
-// const infoSubscriber = new InfoSubscriber(/* referencia al elemento */);
-
-// observer.subscribe(previewSubscriber);
-// observer.subscribe(infoSubscriber);
